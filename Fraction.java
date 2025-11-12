4fbb7f8535be06fbf72fcb20e14677db56361021
xy357 <philip.boettiger@bluewin.ch>
2025-11-12 08:44:29 +0100

    Fraction

--- Fraction.java
+++ Fraction.java
@@ -1,6 +1,6 @@
 public class Fraction {
     static int numberOfFractions;
-    int numerator, denominator;
+    private int numerator, denominator;
     
     @Override
     public String toString() {
@@ -16,13 +16,35 @@ public class Fraction {
     //     this(0, 1);
     // }
 
-    // public void setDenominator(int denominator) {
-    //     if (denominator == 0) {
-    //         throw new ArithmeticException("Division by zero");
-    //     } else {
-    //         this.denominator = denominator;
-    //     }
-    // }
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.setDenominator(denominator);
+    }
+
+
+    public void setDenominator(int denominator) {
+        if (denominator == 0) {
+            throw new ArithmeticException("Division by zero");
+        } else {
+            this.denominator = denominator;
+        }
+    }
+
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+
+    public int getDenominator() {
+        return denominator;
+    }
+    
 
     // public void add(Fraction f) {
     //     this.setNumerator(this.numerator * f.denominator + this.denominator * f.numerator);
--- FractionTester.java
+++ FractionTester.java
@@ -2,9 +2,9 @@ public class FractionTester {
     public static void main(String[] args) {
         Fraction f = new Fraction();
         // Der folgende Code funktioniert nur, wenn die Felder von Fraction nicht privat sind:
-        f.numerator = 12;
-        f.denominator = 0;
-        System.out.println(f.numerator + "/" + f.denominator);
+        f.setNumerator(12);
+        f.setDenominator(0);
+        System.out.println(f.getNumerator() + "/" + f.getDenominator());
         System.out.println(f.toString());
         
         // toString():
